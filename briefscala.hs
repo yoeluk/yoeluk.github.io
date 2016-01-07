e3aef971ee76fd6f05f3d1cf3a53ce4936012f1d
Yoel Garcia Diaz <yoel.garciadiaz@razorfish.com>
2016-01-07 05:18:38 -0500

    up

--- briefscala.hs
+++ briefscala.hs
@@ -13,10 +13,13 @@ main = do
   _ <- shell "cd ../" empty
   _ <- shell "cabal run build" empty
 
+  _ <- shell "cd _site/" empty
   _ <- shell "git status" empty
   _ <- shell "git add --all" empty
   _ <- shell "git commit -m \"Update (`date '+%F %T %Z'`) [ci skip]\"" empty
+  _ <- shell "git push origin master" empty
 
+  _ <- shell "cd ../" empty
   _ <- shell "git status" empty
   _ <- shell "git add _site" empty
   _ <- shell "git commit -m \"Update _site (`date '+%F %T %Z'`) [ci skip]\"" empty
