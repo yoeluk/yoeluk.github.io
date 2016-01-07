5e3efbb5fdeb1af17d488158033e7902b2525f55
Yoel Garcia Diaz <yoel.garciadiaz@razorfish.com>
2016-01-07 04:02:02 -0500

    Update _site (2016-01-07 04:02:02 EST) [ci skip]

--- briefscala.hs
+++ briefscala.hs
@@ -5,23 +5,24 @@
 import Turtle
 
 main = do
+  _ <- shell "git checkout hakyll" empty
+  _ <- shell "git submodule init" empty
+  _ <- shell "git submodule update" empty
 
-  shell "git submodule init" empty
-  shell "git submodule update" empty
-  shell "cd _site/" empty
-  shell "git checkout master" empty
-  shell "cd ../" empty
+  _ <- shell "git status" empty
+  _ <- shell "git add --all" empty
+  _ <- shell "git commit -m \"Update _site (`date '+%F %T %Z'`) [ci skip]\"" empty
 
-  shell "git status" empty
-  shell "git add --all" empty
-  shell "git commit -m \"Update _site (`date '+%F %T %Z'`) [ci skip]\"" empty
+  _ <- shell "cabal run build" empty
 
-  shell "cabal run build" empty
+  _ <- shell "git push origin hakyll" empty
 
-  shell "git push origin hakyll" empty
-
-  shell "cd _site/" empty
-  shell "git status" empty
-  shell "git add --all" empty
-  shell "git commit -m \"Update (`date '+%F %T %Z'`) [ci skip]\"" empty
-  shell "git push origin master" empty
+  _ <- shell "cd _site/" empty
+  _ <- shell "git checkout master" empty
+  _ <- shell "git status" empty
+  _ <- shell "git add --all" empty
+  _ <- shell "git commit -m \"Update (`date '+%F %T %Z'`) [ci skip]\"" empty
+  final <- shell "git push origin master" empty
+  case final of
+      ExitSuccess   -> return ()
+      ExitFailure n -> die ("updateSite " <> " failed with exit code: " <> repr n)
