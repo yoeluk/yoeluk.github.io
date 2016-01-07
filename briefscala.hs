60af1dce480f37db19bbd7627673203e0d73d868
Yoel Garcia Diaz <yoel.garciadiaz@razorfish.com>
2016-01-07 02:51:51 -0500

    Update _site (2016-01-07 02:51:51 EST) [ci skip]

--- briefscala.hs
+++ briefscala.hs
@@ -5,17 +5,20 @@
 import Turtle
 
 main = do
-  shell "git status" empty
-  shell "git add --all" empty
-  shell "git commit -m \"Update _site (`date '+%F %T %Z'`) [ci skip]\"" empty
-  shell "git push origin hakyll" empty
 
   shell "git submodule init" empty
   shell "git submodule update" empty
   shell "cd _site/" empty
   shell "git checkout master" empty
   shell "cd ../" empty
+
+  shell "git status" empty
+  shell "git add --all" empty
+  shell "git commit -m \"Update _site (`date '+%F %T %Z'`) [ci skip]\"" empty
+
   shell "cabal run build" empty
+
+  shell "git push origin hakyll" empty
 
   shell "cd _site/" empty
   shell "git status" empty
