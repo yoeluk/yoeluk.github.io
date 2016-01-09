fb1395c57304b0be92adaf6b6561ba512fdc2e5a
Yoel Garcia Diaz <yoel.garciadiaz@razorfish.com>
2016-01-08 19:11:10 -0500

    Update _site (2016-01-08 19:11:10 EST) [ci skip]

--- briefscala.hs
+++ briefscala.hs
@@ -16,6 +16,7 @@ main = do
 
   _ <- shell "git status" empty
   _ <- shell "git add _site" empty
+  _ <- shell "git add -u" empty
   _ <- shell "git commit -m \"Update _site (`date '+%F %T %Z'`) [ci skip]\"" empty
   final <- shell "git push origin hakyll" empty
   case final of
